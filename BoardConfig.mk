3a12220e632749074c332a994200b0fa68f87377
Brian Petroski <brianpetroski@yahoo.com>
2015-09-14 23:56:38 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,7 +163,6 @@ TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
 #TARGET_ENABLE_UKM := true
 
-WITH_LZMA_OTA := false
 
 #SaberMod
 -include vendor/bliss/config/sm.mk
